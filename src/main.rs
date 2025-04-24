139a7d7dd56d7520c01270046fa6389916f6447c
Juan Abdon <juan.abdon@lambdaclass.com>
2025-04-24 18:54:25 -0300

    Add editing mode
    Editing mode allows users to draw on the screen.
    
    User can switch between editing and running modes by
    pressing space bar.
    
    UI text labels were added to indicate the mode the user
    is currently on and a short help message at the bottom.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use game_of_life::model;
-use macroquad::prelude::*;
+use macroquad::{miniquad::window::screen_size, prelude::*};
 
 const GRID_WIDTH: usize = 50;
 const GRID_HEIGHT: usize = 50;
+
+fn screen_to_grid(screen_pos: (f32, f32)) -> (usize, usize) {
+    let grid_screen_ratio = (
+        GRID_WIDTH as f32 / screen_width(),
+        GRID_HEIGHT as f32 / screen_height(),
+    );
+
+    let x = (grid_screen_ratio.0 * screen_pos.0).floor();
+    let y = (grid_screen_ratio.1 * screen_pos.1).floor();
+
+    (x as usize, y as usize) // Will always be greater than or equal to 0
+}
+
+fn edit_mode(grid: &mut model::Grid) {
+    if !is_mouse_button_pressed(MouseButton::Left) {
+        return;
+    }
+
+    let (x, y) = screen_to_grid(mouse_position());
+    grid.toggle(x, y);
+}
 
 // Updates the state of the current grid given a the previous one
 fn update(prev: &model::Grid, grid: &mut model::Grid) {
@@ -12,30 +33,41 @@ fn update(prev: &model::Grid, grid: &mut model::Grid) {
     }
 }
 
+fn draw_ui(is_editing: &bool) {
+    let (width, height) = screen_size();
+
+    let mode = if *is_editing { "EDITING" } else { "RUNNING" };
+    let mode_label = format!("Mode: {}", mode);
+    draw_text(mode_label.as_str(), width * 0.01, 30.0, 30.0, BLUE);
+
+    let help_label = "Press <space> to toggle in and out of EDITING and RUNNING modes.";
+    draw_text(help_label, width * 0.01, height * 0.97, 20., BLUE);
+}
+
 #[macroquad::main("Game of Life")]
 async fn main() {
     let mut prev_grid = model::Grid::new(GRID_WIDTH, GRID_HEIGHT);
-
-    // Initial state
-    prev_grid.toggle(24, 22);
-    prev_grid.toggle(24, 23);
-    prev_grid.toggle(24, 24);
-    prev_grid.toggle(23, 23);
-    prev_grid.toggle(25, 24);
-
     let mut grid = prev_grid.clone();
+
+    let mut is_editing = true;
 
     loop {
         clear_background(WHITE);
 
-        update(&prev_grid, &mut grid);
+        if is_editing {
+            edit_mode(&mut grid);
+        } else {
+            update(&prev_grid, &mut grid);
+        }
+
+        if is_key_pressed(KeyCode::Space) {
+            is_editing = !is_editing;
+        }
 
         grid.draw();
-
-        let fps_text = format!("FPS: {}", get_fps());
-        draw_text(fps_text.as_str(), 10.0, 30.0, 30.0, GREEN);
-
         prev_grid = grid.clone();
+
+        draw_ui(&is_editing);
 
         // Make game loop slower to be able too see moving objects better
         std::thread::sleep(std::time::Duration::from_millis(30));
